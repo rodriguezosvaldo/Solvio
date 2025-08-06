ba44dfb600a5f12381284ce9eeb9310739c5a1e8
Osvaldo Rodriguez <osvaldo.rguez911001@gmail.com>
2025-08-06 12:57:46 -0400

    Add flowchart

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,7 @@ import CategoriesPieChart from './CategoriesPieChart'
 const Home = () => {
     return (
         <div className='max-sm:flex-col-reverse flex gap-4 w-full h-full overflow-y-auto'>
-            <div className='max-sm:h-3/4 max-sm:gap-1 max-sm:p-0 flex flex-col w-full h-full justify-center items-center gap-4 p-2 overflow-y-auto'>
+            <div className='max-sm:h-3/4 max-sm:gap-1 max-sm:p-0 flex flex-col w-full h-full justify-center items-center gap-4 py-2 overflow-y-auto'>
                 <div className='flex w-full h-1/2 justify-center items-center gap-4 p-2'>
                     <div className='flex w-full h-full justify-center items-center animate-blurred-fade-in duration-300'>
                         <GeneralBudget/>
@@ -24,7 +24,7 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className='max-sm:w-full max-sm:h-1/4 max-sm:p-0 flex w-1/2 h-full justify-center items-center p-2 overflow-y-auto animate-blurred-fade-in duration-300'>
+            <div className='max-sm:w-full max-sm:h-1/4 max-sm:p-0 flex w-1/2 h-full justify-center items-center py-2 overflow-y-auto animate-blurred-fade-in duration-300'>
                 <MainCategories />
             </div>
         </div>
